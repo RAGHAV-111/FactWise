dd9292e3a43fd5bf4aec7ab6ebf77fb1136ae013
Raghav Dwivedi <RAGHAV-111>
2024-02-29 19:44:04 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PeopleList from "./Components/PeopleList";
 
-
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<PeopleList/>} />
-
       </Routes>
     </Router>
   );
